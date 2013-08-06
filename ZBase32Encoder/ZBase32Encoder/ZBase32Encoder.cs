969387ddff6e8cbc5244c9343a1601c072e7a0a7
Deniska <denxc@yandex.ru>
2013-08-07 00:45:39 +0700

    Encoding method was implemented and some tests was created.

---
+++ ZBase32Encoder/ZBase32Encoder/ZBase32Encoder.cs
@@ -0,0 +1,64 @@
+﻿namespace ZBase32Encoder
+{
+    using System;
+    using System.Text;
+
+    public static class ZBase32Encoder
+    {
+        private const string EncodingTable = "ybndrfg8ejkmcpqxot1uwisza345h769";
+
+        private static readonly byte[] DecodintTable = new byte[128];
+
+        static ZBase32Encoder()
+        {
+            for (var i = 0; i < DecodintTable.Length; ++i)
+            {
+                DecodintTable[i] = byte.MaxValue;
+            }
+
+            for (var i = 0; i < EncodingTable.Length; ++i)
+            {
+                DecodintTable[EncodingTable[i]] = (byte)i;
+            }
+        }
+
+        public static string Encode(byte[] data)
+        {
+            if (data == null)
+            {
+                throw new ArgumentNullException("data");
+            }
+
+            var encodedResult = new StringBuilder((int)Math.Ceiling(data.Length * 8.0 / 5.0));
+
+            for (var i = 0; i < data.Length; i += 5)
+            {
+                var byteCount = Math.Min(5, data.Length - i);
+                
+                ulong buffer = 0;
+                for (var j = 0; j < byteCount; ++j)
+                {
+                    buffer = (buffer << 8) | data[i + j];
+                }
+
+                var bitCount = byteCount * 8;
+                while (bitCount > 0)
+                {                    
+                    var index = bitCount >= 5
+                                ? (int)(buffer >> (bitCount - 5)) & 0x1f
+                                : (int)(buffer & (ulong)(0x1f >> (5 - bitCount))) << (5 - bitCount);
+
+                    encodedResult.Append(EncodingTable[index]);
+                    bitCount -= 5;
+                }
+            }
+
+            return encodedResult.ToString();
+        }
+
+        public static byte[] Decode(string data)
+        {
+            return null;
+        }
+    }
+}
---
+++ ZBase32Encoder/ZBase32EncoderTests/ZBase32EncoderTest.cs
@@ -0,0 +1,38 @@
+﻿namespace ZBase32EncoderTests
+{
+    using System.Text;
+
+    using NUnit.Framework;
+
+    using ZBase32Encoder;
+
+    [TestFixture]
+    public class ZBase32EncoderTest
+    {        
+        [TestCase("BA", "ejyo")]
+        [TestCase("a", "cr")]
+        [TestCase("", "")]
+        public void EncodingTest(string sourceData, string encodedData)
+        {
+            var bytes = Encoding.ASCII.GetBytes(sourceData);
+            var result = ZBase32Encoder.Encode(bytes);
+
+            Assert.AreEqual(encodedData, result);
+        }
+
+        [TestCase("Hello, World!")]
+        [TestCase("&^%%&*BJKjbjkb&^%%$^&b")]
+        [TestCase("  My NaMe Is DeNiS ")]
+        [TestCase("--=__--=)(\\//$4")]
+        [TestCase("  ")]
+        [TestCase("")]
+        public void EncodeDecodeTest(string sourceData)
+        {
+            var bytes = Encoding.ASCII.GetBytes(sourceData);
+            var encodedData = ZBase32Encoder.Encode(bytes);
+            var decodedData = ZBase32Encoder.Decode(encodedData);
+
+            Assert.AreEqual(sourceData, decodedData);
+        }
+    }
+}
